4535f00eb47f74d7acf032401f8159c42d93147f
michaelhaaf <michael.haaf@gmail.com>
2024-03-28 00:31:08 -0400

    remove old sddm themeing lines in set theme script
    choose papirus icon

--- config/scripts/settheming.sh
+++ config/scripts/settheming.sh
@@ -5,12 +5,6 @@ set -oue pipefail
 
 gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark' 
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-gsettings set org.gnome.desktop.interface icon-theme 'breeze-dark'
+gsettings set org.gnome.desktop.interface icon-theme 'papirus-dark'
 
 sed -i 's/Inherits=Adwaita/Inherits=ePapirus/' /usr/share/icons/default/index.theme 
-sed -i 's/color: "black"/color: "white"/' /usr/share/sddm/themes/maldives/Main.qml
-sed -i 's/id: lblPassword/id: lblPassword\ncolor: "white"/' /usr/share/sddm/themes/maldives/Main.qml
-sed -i 's/id: lblName/id: lblName\ncolor: "white"/' /usr/share/sddm/themes/maldives/Main.qml
-sed -i 's/id: lblSession/id: lblSession\ncolor: "white"/' /usr/share/sddm/themes/maldives/Main.qml
-sed -i 's/id: lblLayout/id: lblLayout\ncolor: "white"/' /usr/share/sddm/themes/maldives/Main.qml
-sed -i 's/id: errorMessage/id: errorMessage\ncolor: "white"/' /usr/share/sddm/themes/maldives/Main.qml
