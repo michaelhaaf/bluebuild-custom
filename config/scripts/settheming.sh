2ba8a208d54c270462f3f82edc5bb4d3e7acad64
michaelhaaf <michael.haaf@gmail.com>
2024-03-28 00:53:55 -0400

    settheming script overhaul: extract theme archives

--- config/scripts/settheming.sh
+++ config/scripts/settheming.sh
@@ -8,3 +8,11 @@ gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.interface icon-theme 'papirus-dark'
 
 sed -i 's/Inherits=Adwaita/Inherits=ePapirus/' /usr/share/icons/default/index.theme 
+
+unzip -q /usr/local/share/fonts/et-book.zip -d /usr/local/share/fonts/
+tar -xf /usr/share/themes/adw-gtk3v5.3.tar.xz -C /usr/share/themes/
+tar -xzf /usr/share/icons/Papirus.tar.gz -C /usr/share/icons/
+tar -xzf /usr/share/icons/Papirus-Dark.tar.gz -C /usr/share/icons/
+tar -xzf /usr/share/icons/Papirus-Light.tar.gz -C /usr/share/icons/
+tar -xzf /usr/share/icons/ePapirus.tar.gz -C /usr/share/icons/
+tar -xzf /usr/share/icons/ePapirus-Dark.tar.gz -C /usr/share/icons/
