865a02bafa5fce81560289d1fb334856eac30fa0
michaelhaaf <michael.haaf@gmail.com>
2024-03-30 16:12:54 -0400

    refactor: whitespace

--- config/scripts/settheming.sh
+++ config/scripts/settheming.sh
@@ -3,11 +3,11 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark' 
+gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark'
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.interface icon-theme 'papirus-dark'
 
-sed -i 's/Inherits=Adwaita/Inherits=ePapirus/' /usr/share/icons/default/index.theme 
+sed -i 's/Inherits=Adwaita/Inherits=ePapirus/' /usr/share/icons/default/index.theme
 
 unzip -q /usr/local/share/fonts/et-book.zip -d /usr/local/share/fonts/
 tar -xf /usr/share/themes/adw-gtk3v5.3.tar.xz -C /usr/share/themes/
