bbf273d1747eb98e426c49635f54b226293c6373
michaelhaaf <michael.haaf@gmail.com>
2024-04-14 15:06:29 -0400

    fix: wrong path for fontconfig preset symlinks

--- config/scripts/settheming.sh
+++ config/scripts/settheming.sh
@@ -10,8 +10,8 @@ gsettings set org.gnome.desktop.interface icon-theme 'papirus-dark'
 sed -i 's/Inherits=Adwaita/Inherits=ePapirus/' /usr/share/icons/default/index.theme
 
 unzip -q /usr/share/fonts/et-book.zip -d /usr/share/fonts/
-ln -s /usr/share/fontconfig/conf.avail/50-user.conf /usr/share/etc/conf.d
-ln -s /usr/share/fontconfig/conf.avail/51-local.conf /usr/share/etc/conf.d
+ln -s /usr/share/fontconfig/conf.avail/50-user.conf /usr/etc/fonts/conf.d
+ln -s /usr/share/fontconfig/conf.avail/51-local.conf /usr/etc/fonts/conf.d
 rm /usr/share/etc/conf.d/.gitkeep
 
 tar -xf /usr/share/themes/adw-gtk3v5.3.tar.xz -C /usr/share/themes/
