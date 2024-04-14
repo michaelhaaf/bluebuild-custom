41d95102da00b19907c5c03b2ad2163c518cec02
michaelhaaf <michael.haaf@gmail.com>
2024-04-14 14:56:27 -0400

    fix attempt: fontconfig fixes, preset symlinks

--- config/scripts/settheming.sh
+++ config/scripts/settheming.sh
@@ -10,6 +10,10 @@ gsettings set org.gnome.desktop.interface icon-theme 'papirus-dark'
 sed -i 's/Inherits=Adwaita/Inherits=ePapirus/' /usr/share/icons/default/index.theme
 
 unzip -q /usr/share/fonts/et-book.zip -d /usr/share/fonts/
+ln -s /usr/share/fontconfig/conf.avail/50-user.conf /usr/share/etc/conf.d
+ln -s /usr/share/fontconfig/conf.avail/51-local.conf /usr/share/etc/conf.d
+rm /usr/share/etc/conf.d/.gitkeep
+
 tar -xf /usr/share/themes/adw-gtk3v5.3.tar.xz -C /usr/share/themes/
 tar -xzf /usr/share/icons/Papirus.tar.gz -C /usr/share/icons/
 tar -xzf /usr/share/icons/Papirus-Dark.tar.gz -C /usr/share/icons/
