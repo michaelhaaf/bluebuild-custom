a022ec64b62776db3018ea7e183363c168c0b486
michaelhaaf <michael.haaf@gmail.com>
2024-09-18 14:37:08 -0400

    fix /usr/etc settheming.sh

--- files/scripts/settheming.sh
+++ files/scripts/settheming.sh
@@ -10,9 +10,9 @@ gsettings set org.gnome.desktop.interface icon-theme 'papirus-dark'
 sed -i 's/Inherits=Adwaita/Inherits=ePapirus/' /usr/share/icons/default/index.theme
 
 unzip -q /usr/share/fonts/et-book.zip -d /usr/share/fonts/
-ln -s /usr/share/fontconfig/conf.avail/50-user.conf /usr/etc/fonts/conf.d
-ln -s /usr/share/fontconfig/conf.avail/51-local.conf /usr/etc/fonts/conf.d
-rm /usr/etc/fonts/conf.d/.gitkeep
+ln -s /usr/share/fontconfig/conf.avail/50-user.conf /etc/fonts/conf.d
+ln -s /usr/share/fontconfig/conf.avail/51-local.conf /etc/fonts/conf.d
+rm /etc/fonts/conf.d/.gitkeep
 
 tar -xf /usr/share/themes/adw-gtk3v5.3.tar.xz -C /usr/share/themes/
 tar -xzf /usr/share/icons/Papirus.tar.gz -C /usr/share/icons/
