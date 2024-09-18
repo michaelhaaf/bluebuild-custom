7bb78aff5e563227792614d7cd20ad3027db7523
michaelhaaf <michael.haaf@gmail.com>
2024-09-18 15:06:13 -0400

    fix: /etc/fonts/conf.d/* files already exist (unlike /usr/etc ones before)

--- files/scripts/settheming.sh
+++ files/scripts/settheming.sh
@@ -10,8 +10,6 @@ gsettings set org.gnome.desktop.interface icon-theme 'papirus-dark'
 sed -i 's/Inherits=Adwaita/Inherits=ePapirus/' /usr/share/icons/default/index.theme
 
 unzip -q /usr/share/fonts/et-book.zip -d /usr/share/fonts/
-ln -s /usr/share/fontconfig/conf.avail/50-user.conf /etc/fonts/conf.d
-ln -s /usr/share/fontconfig/conf.avail/51-local.conf /etc/fonts/conf.d
 rm /etc/fonts/conf.d/.gitkeep
 
 tar -xf /usr/share/themes/adw-gtk3v5.3.tar.xz -C /usr/share/themes/
